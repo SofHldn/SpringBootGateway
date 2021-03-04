380887953cf4bf476e2da5e737baaf69ee39d0fe
SofHldn <sofie.holden@iths.se>
2021-03-04 15:53:37 +0100

    first commit

---
+++ src/main/java/se/iths/gatewayspringlab/GatewayspringlabApplication.java
@@ -0,0 +1,13 @@
+package se.iths.gatewayspringlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayspringlabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayspringlabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/gatewayspringlab/GatewayspringlabApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.gatewayspringlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayspringlabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
